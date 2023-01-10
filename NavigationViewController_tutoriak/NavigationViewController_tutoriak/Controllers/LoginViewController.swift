be78cf9d6d97c9a7ecccc91990ba2ee5c2c62e2c
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-01-10 23:20:16 +0900

    test

---
+++ NavigationViewController_tutoriak/NavigationViewController_tutoriak/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationViewController_tutoriak
+//
+//  Created by KyungHeon Lee on 2023/01/10.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NavigationViewController_tutoriak/NavigationViewController_tutoriak/Controllers/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  NavigationViewController_tutoriak
+//
+//  Created by KyungHeon Lee on 2023/01/10.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
