6a6a0e6187e66c0b800dabda777f4e66af3926b0
BOLTB0X <khunlee0508@gmail.com>
2023-02-06 23:37:04 +0900

    image setting

---
+++ Debugging/Debugging/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Debugging
+//
+//  Created by KyungHeon Lee on 2023/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
