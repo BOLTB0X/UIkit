23783b2e97eb1173ef06920e2470346ba37dd0d7
BOLTB0X <khunlee0508@gmail.com>
2023-02-08 23:47:01 +0900

    initplayer add

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -34,5 +34,24 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         print("slider Value changed")
     }
     
+    // custom player 초기화
+    func initializePlayer() {
+        
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올 수 없음")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메세지 : \(error.localizedDescription)")
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
 }
-
