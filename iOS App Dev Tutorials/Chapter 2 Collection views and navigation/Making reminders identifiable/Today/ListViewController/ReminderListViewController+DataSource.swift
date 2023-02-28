f2af43dada18acb313f93cd43c181973ffc07b39
BOLTB0X <khunlee0508@gmail.com>
2023-02-28 23:06:58 +0900

    section commit

--- iOS App Dev Tutorials/Chapter 2 Collection views and navigation/Making reminders identifiable/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ iOS App Dev Tutorials/Chapter 2 Collection views and navigation/Making reminders identifiable/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -8,6 +8,14 @@ extension ReminderListViewController {
     // ReminderListViewController+DataSource.swift에서 데이터 소스의 항목 식별자 유형과 스냅샷 유형 별칭을 Reminder.ID로 변경
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
+    
+    // 셀 등록 처리기에서 올바른 지역화된 문자열을 셀의 액세스 가능성 값에 할당
+    var reminderCompletedValue: String {
+        NSLocalizedString("Completed", comment: "Reminder completed value")
+    }
+    var reminderNotCompletedValue: String {
+        NSLocalizedString("Not completed", comment: "Reminder not completed value")
+    }
 
     // 미리 알림 목록 보기 컨트롤러의 viewDidLoad() 메서드에서 이전 단계에서 생성한 메서드로 스냅샷 코드를 추출
     // 업데이트할 미리 알림 식별자 배열을 허용하도록 updateSnapshot()을 수정
@@ -70,6 +78,22 @@ extension ReminderListViewController {
         // 앱을 빌드 및 실행하고 완료 버튼을 탭하여 미리 알림의 완료 상태를 변경
         // completeReminder(withId:)에서 스냅샷을 업데이트할 때 미리 알림의 식별자를 전달
         updateSnapshot(reloading: [id])
+    }
+    
+    // 미리 알림에 대한 UIAccessibilityCustomAction을 생성하는 doneButtonAccessibilityAction(for:) 메서드를 추가
+    // 셀 등록 처리기에서 이 메서드를 호출하여 각 셀에 대한 사용자 지정 작업을 만듬
+    private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction
+    {
+        let name = NSLocalizedString(
+                 "Toggle completion", comment: "Reminder done button accessibility label")
+        // actionHandler 클로저에서 true를 반환
+        // 기본적으로 클로저는 내부에서 사용하는 외부 값에 대한 강력한 참조를 생성
+        // 뷰 컨트롤러에 대한 약한 참조를 지정하면 유지 주기가 방지
+        let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
+            self?.completeReminder(withId: reminder.id)
+            return true
+        }
+        return action
     }
 
     private func doneButtonConfiguration(for reminder: Reminder)
