58a1b0bb2327c1c76a2ad5c9e385e3ab92f2fc11
BOLTB0X <khunlee0508@gmail.com>
2023-03-04 23:14:28 +0900

    button count commit

--- self-taught graffiti/practiceButton/practiceButton/ViewController.swift
+++ self-taught graffiti/practiceButton/practiceButton/ViewController.swift
@@ -8,18 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+        
+    // 카운트 변수
+    var cnt: Int = 0
     
-    var check:Bool = false
+    @IBOutlet weak var textLabel: UILabel!
 
+    @IBOutlet weak var btnP: UIButton!
+    
+    @IBOutlet weak var btnN: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 방법 1
+        textLabel.text = "\(textLabel)"
+        
+        // 레이벨 방법 2
+        textLabel.text = String(format: "%d", cnt)
+        
+        btnP.addTarget(self, action: #selector(addAct(_:)), for: .touchUpInside)
+        
+        btnN.addTarget(self, action: #selector(subAct(_:)), for: .touchUpInside)
     }
 
-    @IBAction func clickButton(_ sender: Any) {
-        print("push")
-        check.toggle()
+    @IBAction func addAct(_ sender: Any) {
+        // 버튼이 눌렸을 경우
+        cnt += 1
+        textLabel.text = "\(cnt)"
     }
     
+    @IBAction func subAct(_ sender: Any) {
+        // 버튼이 눌렸을 경우
+        cnt -= 1
+        textLabel.text = "\(cnt)"
+    }
 }
 
