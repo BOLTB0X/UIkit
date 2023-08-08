bb3c0af2f4297095ce9892c606fb837987a18dbc
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-08-08 16:49:33 +0900

    Display the header view

---
+++ iOS App Dev Tutorials/Chapter 5 Custom views and animation/01_Creating a progress view/StartingProject/Today/ListViewController/ReminderListViewController.swift
@@ -0,0 +1,114 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    var dataSource: DataSource!
+    var reminders: [Reminder] = Reminder.sampleData
+    var listStyle: ReminderListStyle = .today
+    var filteredReminders: [Reminder] {
+        return reminders.filter { listStyle.shouldInclude(date: $0.dueDate) }.sorted {
+            $0.dueDate < $1.dueDate
+        }
+    }
+    let listStyleSegmentedControl = UISegmentedControl(items: [
+        ReminderListStyle.today.name, ReminderListStyle.future.name, ReminderListStyle.all.name
+    ])
+    
+    // add
+    var headerView: ProgressHeaderView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        // viewDidLoad()에서 header view를 supplementary view로 등록
+        let headerRegistration = UICollectionView.SupplementaryRegistration(elementKind: ProgressHeaderView.elementKind, handler: supplementaryRegistrationHandler)
+        /// 새 header view등록을 사용하는 데이터 소스에 보충 보기 공급자 클로저를 추가
+        /// 이 클로저는 diff 가능한 데이터 소스에서 보충 헤더 뷰를 구성하고 반환
+        dataSource.supplementaryViewProvider = { supplementaryView, elementKind, indexPath in
+            return self.collectionView.dequeueConfiguredReusableSupplementary(
+                using: headerRegistration, for: indexPath)
+        }
+
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: #selector(didPressAddButton(_:)))
+        addButton.accessibilityLabel = NSLocalizedString(
+            "Add reminder", comment: "Add button accessibility label")
+        navigationItem.rightBarButtonItem = addButton
+
+        listStyleSegmentedControl.selectedSegmentIndex = listStyle.rawValue
+        listStyleSegmentedControl.addTarget(
+            self, action: #selector(didChangeListStyle(_:)), for: .valueChanged)
+        navigationItem.titleView = listStyleSegmentedControl
+
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+
+        updateSnapshot()
+
+        collectionView.dataSource = dataSource
+    }
+
+    override func collectionView(
+        _ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let id = filteredReminders[indexPath.item].id
+        pushDetailViewForReminder(withId: id)
+        return false
+    }
+
+    func pushDetailViewForReminder(withId id: Reminder.ID) {
+        let reminder = reminder(withId: id)
+        let viewController = ReminderViewController(reminder: reminder) { [weak self] reminder in
+            self?.updateReminder(reminder)
+            self?.updateSnapshot(reloading: [reminder.id])
+        }
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.headerMode = .supplementary // add
+        listConfiguration.showsSeparators = false
+        listConfiguration.trailingSwipeActionsConfigurationProvider = makeSwipeActions
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+
+    private func makeSwipeActions(for indexPath: IndexPath?) -> UISwipeActionsConfiguration? {
+        guard let indexPath = indexPath, let id = dataSource.itemIdentifier(for: indexPath) else {
+            return nil
+        }
+        let deleteActionTitle = NSLocalizedString("Delete", comment: "Delete action title")
+        let deleteAction = UIContextualAction(style: .destructive, title: deleteActionTitle) {
+            [weak self] _, _, completion in
+            self?.deleteReminder(withId: id)
+            self?.updateSnapshot()
+            completion(false)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    // MARK: - supplementaryRegistrationHandler
+    // 파일 맨 아래에 진행률 헤더 view, 요소 종류 및 인덱스 경로를 허용하는 supplementaryRegistrationHandler
+    private func supplementaryRegistrationHandler(
+        progressView: ProgressHeaderView, elementKind: String, indexPath: IndexPath
+    ) {
+        // 들어오는 progressView를 headerView 프로퍼티에 할당
+        headerView = progressView
+    }
+}
